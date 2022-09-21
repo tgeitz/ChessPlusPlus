b01f33b91629b17ea70fd62eedcc5df2db43e0c2
Tom Geitz <tgeitz@gmail.com>
2022-09-21 13:01:12 -0500

    Add backwards pawn move rule and comments for all rules

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -6,6 +6,7 @@ bool Chess::isValidMove(Game game, Game::Move move) {
 	Square& initialSquare = move.startingBoard.getSquareForPosition(move.startingPosition);
 	Piece& piece = initialSquare.getPiece();
 
+	// trying to move an empty square
 	if (piece.type == PieceType::EMPTY) {
 		return false;
 	}
@@ -15,6 +16,7 @@ bool Chess::isValidMove(Game game, Game::Move move) {
 	bool wouldCapture = targetPiece.type != PieceType::EMPTY && piece.type != targetPiece.type;
 
 	if (
+		// trying to move beyond bounds of the board
 		move.intendedPosition.iColumn < 0 || move.intendedPosition.iColumn > 7
 		|| move.intendedPosition.iRow < 0 || move.intendedPosition.iRow > 7
 	) {
@@ -26,9 +28,14 @@ bool Chess::isValidMove(Game game, Game::Move move) {
 
 	if (piece.type == PieceType::PAWN) {
 		if (
-			abs(yDistance) > 2 || abs(yDistance < 1) // moving 0 or more than 2 vertically
-			|| (abs(yDistance) == 2 && (move.startingPosition.iRow != 1 && move.startingPosition.iRow != 6)) // moving 2 when not on starting square
-			|| ((abs(xDistance) == 1 && !wouldCapture) || abs(xDistance) > 1) // not moving 0 horizontally, or moving 1 when not capturing
+			// trying to move 0 or more than 2 vertically
+			abs(yDistance) > 2 || abs(yDistance < 1)
+			// trying to move 2 when not on starting square
+			|| (abs(yDistance) == 2 && (move.startingPosition.iRow != 1 && move.startingPosition.iRow != 6))
+			// pawn is trying to move backwards with respect to its color
+			|| ((piece.color == PieceColor::WHITE && yDistance < 0) || (piece.color == PieceColor::BLACK && yDistance > 0))
+			// not moving 0 horizontally, or moving 1 horizontally when not capturing
+			|| ((abs(xDistance) == 1 && !wouldCapture) || abs(xDistance) > 1)
 		) {
 			return false;
 		}
